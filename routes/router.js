23965f992c635594acdaaa53af7465b8ae747f0e
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2021-01-01 19:05:58 -0300

    Changed routes to more organized subdivisions

--- routes/router.js
+++ routes/router.js
@@ -33,65 +33,61 @@ router.get('/api/status', async (_req, res, _next) => res.sendStatus(200));
 /**
  * @description Rutas dolar
  */
-router.get('/api/dolaroficial', dolarInstance.getDolarOficial)
-router.get('/api/dolarblue', dolarInstance.getDolarBlue)
-router.get('/api/contadoliqui', dolarInstance.getContadoConLiqui)
-router.get('/api/dolarpromedio', dolarInstance.getDolarPromedio)
-router.get('/api/dolarbolsa', dolarInstance.getDolarBolsa)
+router.get('/api/dolar/oficial', dolarInstance.getDolarOficial)
+router.get('/api/dolar/blue', dolarInstance.getDolarBlue)
+router.get('/api/dolar/contadoliqui', dolarInstance.getContadoConLiqui)
+router.get('/api/dolar/promedio', dolarInstance.getDolarPromedio)
+router.get('/api/dolar/bolsa', dolarInstance.getDolarBolsa)
 
 /**
  * @description Rutas bancos
  */
-router.get('/api/bbva', bancoInstance.getDolarBBVA)
-router.get('/api/piano', bancoInstance.getDolarPiano)
-router.get('/api/hipotecario', bancoInstance.getDolarHipotecario)
-router.get('/api/galicia', bancoInstance.getDolarGalicia)
-router.get('/api/santander', bancoInstance.getDolarSantander)
-router.get('/api/ciudad', bancoInstance.getDolarCiudad)
-router.get('/api/supervielle', bancoInstance.getDolarSupervielle)
-router.get('/api/patagonia', bancoInstance.getDolarPatagonia)
-router.get('/api/comafi', bancoInstance.getDolarComafi)
-router.get('/api/nacion', bancoInstance.getDolarNacion)
-router.get('/api/bind', bancoInstance.getDolarBIND)
-router.get('/api/chaco', bancoInstance.getDolarChaco)
-router.get('/api/pampa', bancoInstance.getDolarPampa)
-router.get('/api/bancor', bancoInstance.getDolarBancor)
-
-/**
- * @description Ruta riesgo pais
- */
-router.get('/api/riesgopais', riesgoInstance.getRiesgoPais)
+router.get('/api/dolar/bancos/bbva', bancoInstance.getDolarBBVA)
+router.get('/api/dolar/bancos/piano', bancoInstance.getDolarPiano)
+router.get('/api/dolar/bancos/hipotecario', bancoInstance.getDolarHipotecario)
+router.get('/api/dolar/bancos/galicia', bancoInstance.getDolarGalicia)
+router.get('/api/dolar/bancos/santander', bancoInstance.getDolarSantander)
+router.get('/api/dolar/bancos/ciudad', bancoInstance.getDolarCiudad)
+router.get('/api/dolar/bancos/supervielle', bancoInstance.getDolarSupervielle)
+router.get('/api/dolar/bancos/patagonia', bancoInstance.getDolarPatagonia)
+router.get('/api/dolar/bancos/comafi', bancoInstance.getDolarComafi)
+router.get('/api/dolar/bancos/nacion', bancoInstance.getDolarNacion)
+router.get('/api/dolar/bancos/bind', bancoInstance.getDolarBIND)
+router.get('/api/dolar/bancos/chaco', bancoInstance.getDolarChaco)
+router.get('/api/dolar/bancos/pampa', bancoInstance.getDolarPampa)
+router.get('/api/dolar/bancos/bancor', bancoInstance.getDolarBancor)
 
 /**
  * @description Rutas euro
  */
-router.get('/api/euro/nacion', euroInstance.getEuroNacion)
-router.get('/api/euro/galicia', euroInstance.getEuroGalicia)
-router.get('/api/euro/bbva', euroInstance.getEuroBBVA)
-router.get('/api/euro/pampa', euroInstance.getEuroPampa)
-router.get('/api/euro/chaco', euroInstance.getEuroChaco)
-router.get('/api/euro/hipotecario', euroInstance.getEuroHipotecario)
+router.get('/api/euro/bancos/nacion', euroInstance.getEuroNacion)
+router.get('/api/euro/bancos/galicia', euroInstance.getEuroGalicia)
+router.get('/api/euro/bancos/bbva', euroInstance.getEuroBBVA)
+router.get('/api/euro/bancos/pampa', euroInstance.getEuroPampa)
+router.get('/api/euro/bancos/chaco', euroInstance.getEuroChaco)
+router.get('/api/euro/bancos/hipotecario', euroInstance.getEuroHipotecario)
 
 /**
  * @description Rutas real
  */
-router.get('/api/real/nacion', realInstance.getRealNacion)
-router.get('/api/real/bbva', realInstance.getRealBBVA)
-router.get('/api/real/chaco', realInstance.getRealChaco)
+router.get('/api/real/bancos/nacion', realInstance.getRealNacion)
+router.get('/api/real/bancos/bbva', realInstance.getRealBBVA)
+router.get('/api/real/bancos/chaco', realInstance.getRealChaco)
 
 /**
  * @description Rutas Reservas y Circulante
  */
+router.get('/api/bcra/riesgopais', riesgoInstance.getRiesgoPais)
 router.get('/api/bcra/reservas', bcraInstance.getReservas)
 router.get('/api/bcra/circulante', bcraInstance.getCirculante)
 
 /**
  * @description Rutas evolución mensual
  */
-router.get('/api/evolucion/dolaroficial', dolarInstance.getEvolucionDolarOficial)
-router.get('/api/evolucion/dolarblue', dolarInstance.getEvolucionDolarBlue)
-router.get('/api/evolucion/realoficial', realInstance.getEvolucionReal)
-router.get('/api/evolucion/eurooficial', euroInstance.getEvolucionEuro)
+router.get('/api/evolucion/dolar/oficial', dolarInstance.getEvolucionDolarOficial)
+router.get('/api/evolucion/dolar/blue', dolarInstance.getEvolucionDolarBlue)
+router.get('/api/evolucion/real/oficial', realInstance.getEvolucionReal)
+router.get('/api/evolucion/euro/oficial', euroInstance.getEvolucionEuro)
 
 
 module.exports = router
