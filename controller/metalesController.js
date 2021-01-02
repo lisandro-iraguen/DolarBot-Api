ffb056da1f5533a730834f8dd372414d50faf485
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2021-01-02 01:28:54 -0300

    Added new endpoints for gold/usd, silver/usd, copper/usd

---
+++ controller/metalesController.js
@@ -0,0 +1,73 @@
+const util = require("../util/util")
+
+class metalesController {
+    constructor(dolarSiService) {
+        this.dolarSiService = dolarSiService
+        this.util = new util()
+    }
+
+    /**
+     * @description Obtener el valor en dólares del oro internacional por onza
+     * @returns Un objeto con el valor, la moneda, la unidad, y la fecha y hora de la consulta
+     */
+    getOro = async (_req, res) => {
+        try {
+            const data = await this.dolarSiService.getInfoDolar()
+            const valores = {
+                fecha: this.util.getDateTime(),
+                valor: this.util.formatNumber(data.cotiza.Metales.casa301.compra._text),
+                unidad: 'Onza',
+                moneda: 'USD'
+            }
+
+            res.send(valores)
+        } catch (e) {
+            res.sendStatus(500)
+            console.log(e)
+        }
+    }
+
+    /**
+     * @description Obtener el valor en dólares de la plata por onza
+     * @returns Un objeto con el valor, la moneda, la unidad, y la fecha y hora de la consulta
+     */
+    getPlata = async (_req, res) => {
+        try {
+            const data = await this.dolarSiService.getInfoDolar()
+            const valores = {
+                fecha: this.util.getDateTime(),
+                valor: this.util.formatNumber(data.cotiza.Metales.casa299.compra._text),
+                unidad: 'Onza',
+                moneda: 'USD'
+            }
+
+            res.send(valores)
+        } catch (e) {
+            res.sendStatus(500)
+            console.log(e)
+        }
+    }
+
+    /**
+     * @description Obtener el valor en dólares del cobre por tonelada
+     * @returns Un objeto con el valor, la moneda, la unidad, y la fecha y hora de la consulta
+     */
+    getCobre = async (_req, res) => {
+        try {
+            const data = await this.dolarSiService.getInfoDolar()
+            const valores = {
+                fecha: this.util.getDateTime(),
+                valor: this.util.formatNumber(data.cotiza.Metales.casa300.compra._text),
+                unidad: 'Tonelada',
+                moneda: 'USD'
+            }
+
+            res.send(valores)
+        } catch (e) {
+            res.sendStatus(500)
+            console.log(e)
+        }
+    }
+}
+
+module.exports = metalesController
--- routes/router.js
+++ routes/router.js
@@ -2,28 +2,30 @@ const express = require('express');
 const router = express.Router();
 
 //Services
-const dolarSiService = require('../services/dolarSiService')
-const coinGeckoService = require('../services/coinGeckoService')
+const dolarSiService = require('../services/dolarSiService');
+const coinGeckoService = require('../services/coinGeckoService');
 
-const dolarSiServiceInstance = new dolarSiService()
-const coinGeckoInstance = new coinGeckoService()
+const dolarSiServiceInstance = new dolarSiService();
+const coinGeckoInstance = new coinGeckoService();
 
 //Controllers
-const dolarController = require('../controller/dolarController')
-const bancoController = require('../controller/bancosController')
+const dolarController = require('../controller/dolarController');
+const bancoController = require('../controller/bancosController');
 const riesgoController = require('../controller/riesgoController');
-const euroController = require('../controller/euroController')
-const realController = require('../controller/realController')
-const bcraController = require('../controller/bcraController')
-const cryptoController = require('../controller/cryptoController')
+const euroController = require('../controller/euroController');
+const realController = require('../controller/realController');
+const bcraController = require('../controller/bcraController');
+const metalesController = require('../controller/metalesController');
+const cryptoController = require('../controller/cryptoController');
 
-const dolarInstance = new dolarController(dolarSiServiceInstance)
-const bancoInstance = new bancoController(dolarSiServiceInstance)
-const riesgoInstance = new riesgoController(dolarSiServiceInstance)
-const euroInstance = new euroController(dolarSiServiceInstance)
-const realInstance = new realController(dolarSiServiceInstance)
-const bcraInstance = new bcraController(dolarSiServiceInstance)
-const cryptoInstance = new cryptoController(coinGeckoInstance)
+const dolarInstance = new dolarController(dolarSiServiceInstance);
+const bancoInstance = new bancoController(dolarSiServiceInstance);
+const riesgoInstance = new riesgoController(dolarSiServiceInstance);
+const euroInstance = new euroController(dolarSiServiceInstance);
+const realInstance = new realController(dolarSiServiceInstance);
+const bcraInstance = new bcraController(dolarSiServiceInstance);
+const metalesInstance = new metalesController(dolarSiServiceInstance);
+const cryptoInstance = new cryptoController(coinGeckoInstance);
 
 /**
  * @description Status
@@ -33,71 +35,78 @@ router.get('/api/status', async (_req, res, _next) => res.sendStatus(200));
 /**
  * @description Rutas dolar
  */
-router.get('/api/dolar/oficial', dolarInstance.getDolarOficial)
-router.get('/api/dolar/blue', dolarInstance.getDolarBlue)
-router.get('/api/dolar/contadoliqui', dolarInstance.getContadoConLiqui)
-router.get('/api/dolar/promedio', dolarInstance.getDolarPromedio)
-router.get('/api/dolar/bolsa', dolarInstance.getDolarBolsa)
+router.get('/api/dolar/oficial', dolarInstance.getDolarOficial);
+router.get('/api/dolar/blue', dolarInstance.getDolarBlue);
+router.get('/api/dolar/contadoliqui', dolarInstance.getContadoConLiqui);
+router.get('/api/dolar/promedio', dolarInstance.getDolarPromedio);
+router.get('/api/dolar/bolsa', dolarInstance.getDolarBolsa);
 
 /**
  * @description Rutas bancos
  */
-router.get('/api/dolar/bancos/bbva', bancoInstance.getDolarBBVA)
-router.get('/api/dolar/bancos/piano', bancoInstance.getDolarPiano)
-router.get('/api/dolar/bancos/hipotecario', bancoInstance.getDolarHipotecario)
-router.get('/api/dolar/bancos/galicia', bancoInstance.getDolarGalicia)
-router.get('/api/dolar/bancos/santander', bancoInstance.getDolarSantander)
-router.get('/api/dolar/bancos/ciudad', bancoInstance.getDolarCiudad)
-router.get('/api/dolar/bancos/supervielle', bancoInstance.getDolarSupervielle)
-router.get('/api/dolar/bancos/patagonia', bancoInstance.getDolarPatagonia)
-router.get('/api/dolar/bancos/comafi', bancoInstance.getDolarComafi)
-router.get('/api/dolar/bancos/nacion', bancoInstance.getDolarNacion)
-router.get('/api/dolar/bancos/bind', bancoInstance.getDolarBIND)
-router.get('/api/dolar/bancos/chaco', bancoInstance.getDolarChaco)
-router.get('/api/dolar/bancos/pampa', bancoInstance.getDolarPampa)
-router.get('/api/dolar/bancos/bancor', bancoInstance.getDolarBancor)
+router.get('/api/dolar/bancos/bbva', bancoInstance.getDolarBBVA);
+router.get('/api/dolar/bancos/piano', bancoInstance.getDolarPiano);
+router.get('/api/dolar/bancos/hipotecario', bancoInstance.getDolarHipotecario);
+router.get('/api/dolar/bancos/galicia', bancoInstance.getDolarGalicia);
+router.get('/api/dolar/bancos/santander', bancoInstance.getDolarSantander);
+router.get('/api/dolar/bancos/ciudad', bancoInstance.getDolarCiudad);
+router.get('/api/dolar/bancos/supervielle', bancoInstance.getDolarSupervielle);
+router.get('/api/dolar/bancos/patagonia', bancoInstance.getDolarPatagonia);
+router.get('/api/dolar/bancos/comafi', bancoInstance.getDolarComafi);
+router.get('/api/dolar/bancos/nacion', bancoInstance.getDolarNacion);
+router.get('/api/dolar/bancos/bind', bancoInstance.getDolarBIND);
+router.get('/api/dolar/bancos/chaco', bancoInstance.getDolarChaco);
+router.get('/api/dolar/bancos/pampa', bancoInstance.getDolarPampa);
+router.get('/api/dolar/bancos/bancor', bancoInstance.getDolarBancor);
 
 /**
  * @description Rutas euro
  */
-router.get('/api/euro/bancos/nacion', euroInstance.getEuroNacion)
-router.get('/api/euro/bancos/galicia', euroInstance.getEuroGalicia)
-router.get('/api/euro/bancos/bbva', euroInstance.getEuroBBVA)
-router.get('/api/euro/bancos/pampa', euroInstance.getEuroPampa)
-router.get('/api/euro/bancos/chaco', euroInstance.getEuroChaco)
-router.get('/api/euro/bancos/hipotecario', euroInstance.getEuroHipotecario)
+router.get('/api/euro/bancos/nacion', euroInstance.getEuroNacion);
+router.get('/api/euro/bancos/galicia', euroInstance.getEuroGalicia);
+router.get('/api/euro/bancos/bbva', euroInstance.getEuroBBVA);
+router.get('/api/euro/bancos/pampa', euroInstance.getEuroPampa);
+router.get('/api/euro/bancos/chaco', euroInstance.getEuroChaco);
+router.get('/api/euro/bancos/hipotecario', euroInstance.getEuroHipotecario);
 
 /**
  * @description Rutas real
  */
-router.get('/api/real/bancos/nacion', realInstance.getRealNacion)
-router.get('/api/real/bancos/bbva', realInstance.getRealBBVA)
-router.get('/api/real/bancos/chaco', realInstance.getRealChaco)
+router.get('/api/real/bancos/nacion', realInstance.getRealNacion);
+router.get('/api/real/bancos/bbva', realInstance.getRealBBVA);
+router.get('/api/real/bancos/chaco', realInstance.getRealChaco);
 
 /**
  * @description Rutas Reservas y Circulante
  */
-router.get('/api/bcra/riesgopais', riesgoInstance.getRiesgoPais)
-router.get('/api/bcra/reservas', bcraInstance.getReservas)
-router.get('/api/bcra/circulante', bcraInstance.getCirculante)
+router.get('/api/bcra/riesgopais', riesgoInstance.getRiesgoPais);
+router.get('/api/bcra/reservas', bcraInstance.getReservas);
+router.get('/api/bcra/circulante', bcraInstance.getCirculante);
 
 /**
  * @description Rutas evolución mensual
  */
-router.get('/api/evolucion/dolar/oficial', dolarInstance.getEvolucionDolarOficial)
-router.get('/api/evolucion/dolar/blue', dolarInstance.getEvolucionDolarBlue)
-router.get('/api/evolucion/real/oficial', realInstance.getEvolucionReal)
-router.get('/api/evolucion/euro/oficial', euroInstance.getEvolucionEuro)
+router.get('/api/evolucion/dolar/oficial', dolarInstance.getEvolucionDolarOficial);
+router.get('/api/evolucion/dolar/blue', dolarInstance.getEvolucionDolarBlue);
+router.get('/api/evolucion/real/oficial', realInstance.getEvolucionReal);
+router.get('/api/evolucion/euro/oficial', euroInstance.getEvolucionEuro);
+
+/**
+ * @description Rutas metales
+ */
+router.get('/api/metales/oro', metalesInstance.getOro);
+router.get('/api/metales/plata', metalesInstance.getPlata);
+router.get('/api/metales/cobre', metalesInstance.getCobre);
 
 /**
  * @description Rutas Cryptomonedas
  */
-router.get('/api/crypto/bitcoin', cryptoInstance.getBitcoin)
-router.get('/api/crypto/ethereum', cryptoInstance.getEthereum)
-router.get('/api/crypto/bitcoincash', cryptoInstance.getBitcoinCash)
-router.get('/api/crypto/monero', cryptoInstance.getMonero)
-router.get('/api/crypto/litecoin', cryptoInstance.getLitecoin)
-router.get('/api/crypto/ripple', cryptoInstance.getRipple)
-router.get('/api/crypto/dash', cryptoInstance.getDash)
+router.get('/api/crypto/bitcoin', cryptoInstance.getBitcoin);
+router.get('/api/crypto/ethereum', cryptoInstance.getEthereum);
+router.get('/api/crypto/bitcoincash', cryptoInstance.getBitcoinCash);
+router.get('/api/crypto/monero', cryptoInstance.getMonero);
+router.get('/api/crypto/litecoin', cryptoInstance.getLitecoin);
+router.get('/api/crypto/ripple', cryptoInstance.getRipple);
+router.get('/api/crypto/dash', cryptoInstance.getDash);
 
 module.exports = router
