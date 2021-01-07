b681139477c4b1143c709945eb284696c77f050e
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2021-01-06 21:41:24 -0300

    Update dolarSiService.js

--- services/dolarSiService.js
+++ services/dolarSiService.js
@@ -1,15 +1,16 @@
 const convert = require('xml-js')
 const axios = require('axios')
 
+const baseUrl = "https://www.dolarsi.com/api/dolarSiInfo.xml";
 
 class dolarSiService {
 
     /**
-     * @description Obtener un json parseado con los valores de dolarSi
+     * @description Obtener un json parseado con los valores de DolarSí
      */
     getInfoDolar = async (req, res) => {
         try {
-            const dataDolar = await axios.get("https://www.dolarsi.com/api/dolarSiInfo.xml")
+            const dataDolar = await axios.get(baseUrl)
             const json = convert.xml2json(dataDolar.data, {compact: true, spaces: 4});
             const jsonParsed = JSON.parse(json);
 
