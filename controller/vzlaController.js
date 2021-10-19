659e10d2bec983665cd6dc3f21755374286d32bb
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2021-10-18 22:52:43 -0300

    Fixed values due to DolarToday changes (values shown as points per million now)

--- controller/vzlaController.js
+++ controller/vzlaController.js
@@ -13,18 +13,21 @@ class vzlaController {
     getDolarVzla = async (_req, res) => {
         try {
             const data = await this.dolarTodayService.getInfoVzla();
+            const paralelo = data.USD.dolartoday * 1000000;
+            const promedioBancos = data.USD.promedio_real * 1000000;
+            const cucuta = data.USD.efectivo_cucuta * 1000000;
             const valores = {
                 fecha: this.util.getDateTime(),
-                paralelo: this.util.formatCurrency(data.USD.dolartoday.toString()),
-                bancos: this.util.formatCurrency(data.USD.promedio_real.toString()),
-                cucuta: this.util.formatCurrency(data.USD.efectivo_cucuta.toString()),
+                paralelo: this.util.formatCurrency(paralelo.toString()),
+                bancos: this.util.formatCurrency(promedioBancos.toString()),
+                cucuta: this.util.formatCurrency(cucuta.toString()),
                 moneda: 'USD',
-            }
+            };
 
             res.send(valores)
         } catch (e) {
-            res.sendStatus(500)
-            console.log(e)
+            res.sendStatus(500);
+            console.log(e);
         }
     }
 
@@ -35,18 +38,21 @@ class vzlaController {
     getEuroVzla = async (_req, res) => {
         try {
             const data = await this.dolarTodayService.getInfoVzla();
+            const paralelo = data.EUR.dolartoday * 1000000;
+            const promedioBancos = data.EUR.promedio_real * 1000000;
+            const cucuta = data.EUR.efectivo_cucuta * 1000000;
             const valores = {
                 fecha: this.util.getDateTime(),
-                paralelo: this.util.formatCurrency(data.EUR.dolartoday.toString()),
-                bancos: this.util.formatCurrency(data.EUR.promedio_real.toString()),
-                cucuta: this.util.formatCurrency(data.EUR.efectivo_cucuta.toString()),
+                paralelo: this.util.formatCurrency(paralelo.toString()),
+                bancos: this.util.formatCurrency(promedioBancos.toString()),
+                cucuta: this.util.formatCurrency(cucuta.toString()),
                 moneda: 'EUR',
-            }
+            };
 
-            res.send(valores)
+            res.send(valores);
         } catch (e) {
-            res.sendStatus(500)
-            console.log(e)
+            res.sendStatus(500);
+            console.log(e);
         }
     }
 }
