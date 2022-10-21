71bc569056722d8b1f11f2839645aa037ad4d00f
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2022-10-20 22:08:54 -0300

    Update bluepy base URL to use vercel

--- services/bluePyService.js
+++ services/bluePyService.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-const baseUrl = "https://bluepy.herokuapp.com";
+const baseUrl = "https://bluepy.vercel.app/";
 
 const DOLAR_OFICIAL = '/api/dolar/oficial';
 const DOLAR_BLUE = '/api/dolar/blue';
@@ -16,24 +16,24 @@ class bluePyService {
 
     _fetchData = async (endpoint) => {
         const url = baseUrl + endpoint;
-            const cacheValue = this.cache.get(url);
-            if(cacheValue !== undefined) {
-                return cacheValue;
-            }
-            else {
-                const response = await axios.get(url);
-                this.cache.set(baseUrl, response.data);
-                return response.data;
-            }
+        const cacheValue = this.cache.get(url);
+        if (cacheValue !== undefined) {
+            return cacheValue;
+        }
+        else {
+            const response = await axios.get(url);
+            this.cache.set(baseUrl, response.data);
+            return response.data;
+        }
     }
 
     /**
      * @description Obtener el valor del Dólar oficial
      */
-    getDolarOficial = async (_req, res) => {        
+    getDolarOficial = async (_req, res) => {
         try {
             return this._fetchData(DOLAR_OFICIAL);
-        } catch(e) {
+        } catch (e) {
             console.log(e);
             res.sendStatus(500);
         }
@@ -42,10 +42,10 @@ class bluePyService {
     /**
      * @description Obtener el valor del Dólar blue
      */
-    getDolarBlue = async (_req, res) => {        
+    getDolarBlue = async (_req, res) => {
         try {
             return this._fetchData(DOLAR_BLUE);
-        } catch(e) {
+        } catch (e) {
             console.log(e);
             res.sendStatus(500);
         }
@@ -54,10 +54,10 @@ class bluePyService {
     /**
      * @description Obtener el valor del Euro oficial
      */
-    getEuroOficial = async (_req, res) => {        
+    getEuroOficial = async (_req, res) => {
         try {
             return this._fetchData(EURO_OFICIAL);
-        } catch(e) {
+        } catch (e) {
             console.log(e);
             res.sendStatus(500);
         }
@@ -66,10 +66,10 @@ class bluePyService {
     /**
      * @description Obtener el valor del Euro blue
      */
-     getEuroBlue = async (_req, res) => {        
+    getEuroBlue = async (_req, res) => {
         try {
             return this._fetchData(EURO_BLUE);
-        } catch(e) {
+        } catch (e) {
             console.log(e);
             res.sendStatus(500);
         }
@@ -78,10 +78,10 @@ class bluePyService {
     /**
      * @description Obtener el valor del Real oficial
      */
-     getRealOficial = async (_req, res) => {        
+    getRealOficial = async (_req, res) => {
         try {
             return this._fetchData(REAL_OFICIAL);
-        } catch(e) {
+        } catch (e) {
             console.log(e);
             res.sendStatus(500);
         }
@@ -90,10 +90,10 @@ class bluePyService {
     /**
      * @description Obtener el valor del Real blue
      */
-     getRealBlue = async (_req, res) => {        
+    getRealBlue = async (_req, res) => {
         try {
             return this._fetchData(REAL_BLUE);
-        } catch(e) {
+        } catch (e) {
             console.log(e);
             res.sendStatus(500);
         }
